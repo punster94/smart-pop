944dfa064025078673de868b26f2a3ec2fe76a4a
punster94 <louis@knights.ucf.edu>
2015-04-16 03:38:37 +0000

    Finalize quiz response submissions.

--- workspace/sample_app/app/controllers/response_containers_controller.rb
+++ workspace/sample_app/app/controllers/response_containers_controller.rb
@@ -6,12 +6,18 @@ class ResponseContainersController < ApplicationController
   end
   
   def create
-    binding.pry
     @response_container = ResponseContainer.new(params[:response_container])
     @response_container.student_id = current_user.account.id
     @response_container.quiz_id = params[:quiz_id]
     @response_container.save
-    flash[:success] = "Responses submitted!"
+    params[:response_container][:response].each_with_index do |response, index|
+      res = Response.new()
+      res.multiple_choice_answer = response[1]['multiple_choice_answer']
+      res.question_id = params["#{index}_question_id".to_s]
+      res.response_container_id = @response_container.id
+      res.save if res.multiple_choice_answer
+    end
+    flash[:success] = "Responses Submitted!"
     redirect_to @response_container.quiz.course
   end
   
@@ -22,5 +28,12 @@ class ResponseContainersController < ApplicationController
   def update
     @response_container = ResponseContainer.find(params[:id])
     @response_container.update_attributes(params[:response_container])
+    params[:response_container][:response].each_with_index do |response, index|
+      res = Response.where(response_container_id: params[:id], question_id: params["#{index}_question_id".to_s]).first
+      res.multiple_choice_answer = response[1]['multiple_choice_answer']
+      res.save if res.multiple_choice_answer
+    end
+    flash[:success] = "Responses Updated!"
+    redirect_to @response_container.quiz.course
   end
 end
